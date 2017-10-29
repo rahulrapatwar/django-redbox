f260ff2fbcc67b32981a361d9465eb68ea5b8799
Rahul Rapatwar <rahulrapatwar@yahoo.com>
2017-10-29 04:10:20 -0700

    Movies and Games fetched from database and displayed on the store page. Sorting functionality added

--- redbox/shop/urls.py
+++ redbox/shop/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^games/$', views.games, name='games'),
-    url(r'^movies/$', views.movies, name='movies'),
+    url(r'^games/(?P<sort>\w*)$', views.games, name='games'),
+    url(r'^movies/(?P<sort>\w*)$', views.movies, name='movies'),
 ]
--- redbox/shop/views.py
+++ redbox/shop/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render
 from .models import Game,Movie
 
 # Create your views here.
-def games(request):
-    games = Game.objects.all()
+def games(request,sort):
+    if sort == 'alpha':
+        games = Game.objects.all().order_by('title')
+    elif sort == 'rating':
+        games = Game.objects.all().order_by('-rating')
+    else:
+        games = Game.objects.all()
     return render(request,'shop/store.html',{'games':games})
 
-def movies(request):
-    movies = Movie.objects.all()
+def movies(request,sort):
+    if sort == 'alpha':
+        movies = Movie.objects.all().order_by('title')
+    elif sort == 'rating':
+        movies = Movie.objects.all().order_by('-rating')
+    else:
+        movies = Movie.objects.all()
     return render(request,'shop/store.html',{'movies':movies})
